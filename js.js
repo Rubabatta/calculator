90cdcfad0897c328ee666e21ad13bb1dcd31589d
Rubabatta <rubabatta59@gmail.com>
2026-01-07 11:33:37 +0500

    commit

--- js.js
+++ js.js
@@ -1,25 +1,25 @@
-// Display
+
 const display = document.querySelector('.display');
 
-// Number buttons
+
 const numberButtons = document.querySelectorAll(
   '.zero button, .one button, .two button, .three button, .four button, .five button, .six button, .seven button, .eight button, .nine button'
 );
 
-// Operator buttons
+
 const plusBtn = document.querySelector('.plus button');
 const minusBtn = document.querySelector('.minus button');
 const multiplyBtn = document.querySelector('.multiply button');
 const divideBtn = document.querySelector('.divide button');
 
-// Special buttons
+
 const equalBtn = document.querySelector('.equal button');
 const clearBtn = document.querySelector('.ac button');
 const dotBtn = document.querySelector('.dot button');
 
 let currentInput = "";
 
-// Number click
+
 numberButtons.forEach(function(button){
     button.addEventListener("click", function(){
         currentInput += button.innerText;
@@ -27,7 +27,7 @@ numberButtons.forEach(function(button){
     });
 });
 
-// Operator click
+
 [plusBtn, minusBtn, multiplyBtn, divideBtn].forEach(function(button){
     button.addEventListener("click", function(){
         currentInput += " " + button.innerText + " ";
@@ -35,25 +35,25 @@ numberButtons.forEach(function(button){
     });
 });
 
-// Dot click
+
 dotBtn.addEventListener("click", function(){
     currentInput += ".";
     display.innerText = currentInput;
 });
 
-// Clear click
+
 clearBtn.addEventListener("click", function(){
     currentInput = "";
     display.innerText = "0";
 });
 
-// Equal click
+
 equalBtn.addEventListener("click", function(){
-    // Replace X with * for eval
+    
     let expression = currentInput.replace("X", "*");
     try {
         display.innerText = eval(expression);
-        currentInput = display.innerText; // result can be used for next calculation
+        currentInput = display.innerText; 
     } catch {
         display.innerText = "Error";
         currentInput = "";
